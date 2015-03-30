886eb69cd2704b292e8f7988c52e24dd54d535a3
Serge-Evich <frenkdrebin@ukr.net>
2015-03-30 14:26:42 +0300

    vcv

--- src/main/java/ua/com/wordscount/wordsCount.java
+++ src/main/java/ua/com/wordscount/wordsCount.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,6 +20,7 @@ public class wordsCount {
         return "index";
     }
     @RequestMapping(value = "/getWordsCount", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
     public Map<String, Integer> getWordsCount(@RequestParam String text) {
         Map<String, Integer> map = new HashMap<>();
         map.put(text, 1);
